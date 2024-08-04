c0f36ba40ac6388e9bb18000d9d2dbf594e89cff
ARYANPATILSOLOCODER11 <aryanpformal@gmail.com>
2024-08-04 16:21:38 +0530

    w

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,16 @@
-// Import the necessary modules using ES module syntax
-import nextPWA from 'next-pwa';
+import withPWA from 'next-pwa';
 
-// Define PWA configuration options
-const pwaConfig = {
-  dest: 'public', // Destination directory for the PWA files
-  disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
-  register: true, // Register the PWA service worker
-  skipWaiting: true, // Skip waiting for service worker activation
-};
-
-// Define Next.js configuration object
 const nextConfig = {
-  reactStrictMode: true, // Enable React strict mode for improved error handling
-  swcMinify: true, // Enable SWC minification for improved performance
+  reactStrictMode: true,
+  swcMinify: true,
   compiler: {
-    removeConsole: process.env.NODE_ENV !== 'development', // Remove console.log in production
+    removeConsole: process.env.NODE_ENV !== 'development',
   },
 };
 
-// Export the combined configuration for Next.js with PWA support
-export default nextPWA(pwaConfig)(nextConfig);
+export default withPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
